6fe48043c6826cc056ceec61fd066fe3397e7245
Marlon sarria <masarria_747@unihumboldt.edu.co>
2025-06-15 12:22:48 -0500

    config img

--- api-rest/src/routes/desparasitacionRoutes.js
+++ api-rest/src/routes/desparasitacionRoutes.js
@@ -3,14 +3,25 @@ const router = express.Router();
 const pool = require('../config/database'); // conexión a la base de datos
 
 router.post('/', async (req, res) => {
-  const { nombre, email, telefono, mascota, tipo, mensaje } = req.body;
+  const { nombre, email, telefono, mascota, tipo, mensaje, fecha_desparasitacion } = req.body;
 
   try {
     const [result] = await pool.query(
-      'INSERT INTO desparasitacion (nombre, email, telefono, mascota, tipo, mensaje) VALUES (?, ?, ?, ?, ?, ?)',
-      [nombre, email, telefono, mascota, tipo, mensaje]
+      'INSERT INTO desparasitacion (nombre, email, telefono, mascota, tipo, mensaje, fecha_Des) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [nombre, email, telefono, mascota, tipo, mensaje, fecha_desparasitacion]
     );
-    res.status(201).json({ message: 'Solicitud de desparasitación registrada', id: result.insertId });
+
+    //Guardar en la tabla de citas
+    const [cita] = await pool.query(
+      'INSERT INTO citas (nombre_cliente, nombre_mascota, servicio, fecha_cita, detalle) VALUES (?, ?, ?, ?, ?)',
+      [nombre, mascota, 'desparasitacion', fecha_desparasitacion, mensaje]
+    );
+
+    res.status(201).json({ 
+      message: 'Solicitud de desparasitación registrada',
+      id: result.insertId,
+      id_cita: cita.insertId
+    });
   } catch (error) {
     console.error('Error al guardar solicitud de desparasitación:', error);
     res.status(500).json({ message: 'Error del servidor' });
